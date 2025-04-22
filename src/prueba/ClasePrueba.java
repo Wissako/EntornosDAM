710be86f6f0d1867f970e1e315082d237ae5068e
osboxes <osboxes@Luis-entornos>
2025-04-22 12:12:22 +0200

    hyuhy

--- src/prueba/ClasePrueba.java
+++ src/prueba/ClasePrueba.java
@@ -4,7 +4,8 @@ public class ClasePrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Primer mensaje de nuestro programa");
+		System.out.println("Segundo mensaje de nuestro programa");
 	}
 
 }
